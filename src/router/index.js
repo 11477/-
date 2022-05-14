b6a8358877a32be115b7555a071833f663493684
cYw <1147701986@qq.com>
2022-05-14 17:10:00 +0800

    wo shi sha bi

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/user/UserView')
+    component: () => import(/* webpackChunkName: "about" */ '../views/user/UserView'),
+    meta:{
+      unShowNav:true
+    }
   },
   {
     path: '/video',
--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="login" class="login">
-    <div class="title">短视频分享网站</div>
+    <div class="title">ShareVideo</div>
     <div class="login-word">
       <span class="word" style="font-size: 38px;">登录</span>
     </div>
-    <div class="login-box">
-      <div class="login-left">
-
+    <div class="login-box-out">
+      <div class="login-box">
+        <div class="email">
+          <el-input v-model="input1" placeholder="你的邮箱" clearable></el-input>
+        </div>
+        <div class="password">
+          <el-input placeholder="密码" v-model="input2" show-password></el-input>
+        </div>
+        <div class="register-text">
+          <p @click="handleCommand">没有账号？现在注册</p>
+        </div>
+      </div >
+      <el-button type="primary" @click="login" class="login-btn" round size="medium">登 录</el-button>
       </div>
-      <div class="line"></div>
-      <div class="login-right">
-
-      </div>
-    </div>
   </div>
 </template>
 
@@ -25,11 +30,15 @@ export default {
     },
     logout(){
       this.$store.commit("logout");
+    },
+    handleCommand(){
+      this.$router.push("/register");
     }
   },
   data() {
     return {
-      input: ''
+      input1: '',
+      input2: ''
     }
   }
 }
@@ -46,12 +55,29 @@ export default {
   position: fixed;
   text-align: center;
 }
+
+.login-box-out{
+  display: inline-block;
+  margin-top: 50px;
+}
+
+.login-box{
+  width: 550px;
+  background: rgba(255,255,255,0.6);
+  margin-top: 10px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  flex-direction: column;
+}
+
 #login .title{
   background: rgba(255,255,255,0.8);
   position: relative;
   top: 20px;
   font-size: 50px;
-  font-weight: 100;
+  font-weight: 2500;
   line-height: 80px;
   width: 30%;
   height: 80px;
@@ -59,14 +85,29 @@ export default {
   display: inline-block;
 }
 
+.login-box .email{
+  width: 500px;
+  height: 80px;
+  margin-top: 40px;
+  margin-bottom: -20px;
+}
+
 .login-word{
-  width: 1500px;
+  width: 1000px;
   height: 28px;
   margin: 0 auto;
   border-bottom: 1px solid #ddd;
-  margin-top: 30px;
+  margin-top: 50px;
   margin-bottom: 28px;
   text-align: center;
+}
+
+.password{
+  width: 500px;
+  height: 80px;
+  /*display: inline-block;*/
+  margin-top:20px;
+
 }
 
 .login-word .word{
@@ -80,17 +121,20 @@ export default {
   text-align: center;
 }
 
-.login-box{
-  position: relative;
-  width: 980px;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: nowrap;
+.register-text {
+  font-size:14px;
+  line-height:10px;
+  color:#000;
+  text-align: right;
+  margin-bottom: 10px;
+  margin-left: 370px;
+  cursor : pointer;
 }
 
-.login-box .line{
-  border-right: 1px solid #ddd;
-  height: 300px;
-  margin-top: 28px;
+.login-btn{
+  margin-top: 50px;
+  width: 110px;
+  height: 50px;
 }
+
 </style>
