8b8f0ac47b492144781da393b71aa669c8839747
陈百铭 <957167412@qq.com>
2022-05-14 17:03:20 +0800

    nav-Bar更新

--- src/views/search/SearchView.vue
+++ src/views/search/SearchView.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="search">
-    <h1>This is an search page</h1>
-    <h1>This is an search page</h1>
-    <h1>This is an search page</h1>
-    <h1>This is an search page</h1>
-    <h1>This is an search page</h1>
+  <div id="search-view">
+    <el-col :span="700">
+      <el-input
+          id="search"
+          v-model="input1"
+          placeholder="请输入关键字搜索"
+          @focus="Searching"
+      ></el-input>
+    </el-col>
+    <el-col :span="1">
+      <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    </el-col>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SearchView"
+  name: "SearchView",
+  data() {
+    return {
+      input1: '',
+      isSearching: false
+    }
+  },
+  methods:{
+    Searching: function (){
+      this.isSearching = true;
+    }
+  }
 }
 </script>
 
-<style scoped>
 
+<style>
+#search-view {
+  margin-top: 20px;
+  width: 50%;
+  margin-left: 30%;
+}
 </style>
