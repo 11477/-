25d0f5edbdecf4d0772db657efd7206ed541b578
cYw <1147701986@qq.com>
2022-05-19 21:21:30 +0800

    wo shi sha bi

---
+++ src/components/videopage/videopage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="videoCover">
+
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "videoCover",
+}
+</script>
+
+<style scoped>
+
+</style>
