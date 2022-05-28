e320bcd0ed704f9b39787e23d08c662096e563b8
cYw <1147701986@qq.com>
2022-05-28 16:27:43 +0800

    wo shi sha bi

--- src/views/index/HomeView.vue
+++ src/views/index/HomeView.vue
@@ -3,6 +3,11 @@
     <div class="img-box">
 <!--      <img src="">-->
     </div>
+    <div class="video-channel">
+        <a class="channel-link">分区1</a>
+        <a class="channel-link">分区2</a>
+        <a class="channel-link">分区3</a>
+    </div>
     <div class="video-box">
       <VideoCover class="video-cover">
       </VideoCover>
@@ -24,5 +29,26 @@ export default {
   margin-left: 50px;
 }
 
+.video-channel{
+  display: flex;
+  align-items: center;
+  background: white;
+  width: 800px;
+  height:20px;
+}
+
+.channel-link{
+  display: inline-block;
+  box-sizing: content-box;
+  width: 100%;
+  height: 100%;
+  border: 1px solid white;
+  border-radius: 6px;
+  background-color: gray;
+  color: black;
+  text-align: center;
+  font-weight: 400;
+  transition: background-color .3s,color .3s;
+}
 
 </style>
