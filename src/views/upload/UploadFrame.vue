2b5ee35c4df51b60c50bcd5d2a1bbc71c77fac6b
nohesitate <20373441@buaa.edu.cn>
2022-05-31 02:38:04 +0800

    hide secret info

--- src/views/upload/UploadFrame.vue
+++ src/views/upload/UploadFrame.vue
@@ -114,8 +114,8 @@ import user from "@/store/user";
 const COS = require('cos-js-sdk-v5')
 // 填写自己腾讯云cos中的key和id (密钥)
 const cosImg = new COS({
-  SecretId: 'AKIDPqGanv11rZD3sgYK5nTPEya5UDnNwgry', // 身份识别ID
-  SecretKey: 'MzdDnOwNhu07ISXbZPD0JWn7UxPyaIzg' // 身份秘钥
+  SecretId: '***', // 身份识别ID
+  SecretKey: '***' // 身份秘钥
 })
 
 export default {
